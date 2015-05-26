f90135a8b12230a1a8528b096a7219ab8db5594a
holleezzz <hollerupholleezzz@gmail.com>
2015-05-26 12:47:19 +0200

    WebService

---
+++ WebSite4/App_Code/TextFileModel.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.IO;
+
+
+public class TextFileModel
+{
+    DirectoryInfo d = new DirectoryInfo(@"C:\Users\Hollerup\Documents\Visual Studio 2013\Projects\WebSite4\TextFiler\");
+
+
+	public void ChangeTxtFile(string fileText, string filename)
+    {
+        FileInfo[] Files = d.GetFiles("*.txt");
+
+        foreach (FileInfo file in Files)
+        {
+            if (file.Name.Equals(filename))
+            {
+                try
+                {
+                    using (StreamWriter writer = file.CreateText())
+                    {
+                        writer.WriteLine(fileText);
+                    }
+                }
+                catch (FileNotFoundException e)
+                {
+                    throw e;
+                }
+            }
+        }
+	}
+
+    public string getTxtFile(string filename)
+    {
+        FileInfo[] Files = d.GetFiles("*.txt");
+        String text = null;
+
+        foreach (FileInfo file in Files)
+        {
+            if (file.Name.Equals(filename))
+            {
+                try
+                {
+                    using (StreamReader read = file.OpenText())
+                    {
+                        text = read.ReadToEnd();
+                        return text;
+                    }
+                }
+                catch (FileNotFoundException e)
+                {
+                    throw e;
+                }
+            }
+        }
+        return text;
+    }
+
+    public List<string> getAllFileNames()
+    {
+        DirectoryInfo d = new DirectoryInfo(@"C:\Users\Hollerup\Documents\Visual Studio 2013\Projects\WebSite4\TextFiler\");
+        List<string> filenames = new List<string>();
+        FileInfo[] Files;
+
+        try
+        {
+            Files = d.GetFiles("*.txt");
+        }
+        catch(FileNotFoundException e)
+        {
+            throw e;
+        }
+        if (Files != null)
+        {
+            foreach (FileInfo file in Files)
+            {
+                filenames.Add(file.Name);
+            }
+        }
+       
+        return filenames;
+    }
+}
---
+++ WebSite4/App_Code/WebService.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.Services.Protocols;
+using System.IO;
+using System.Data;
+using System.Data.SqlClient;
+
+[WebService(Namespace = "http://SuperErdogan.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+
+public class WebService : System.Web.Services.WebService {
+
+    DirectoryInfo d = new DirectoryInfo(@"C:\Users\Hollerup\Documents\Visual Studio 2013\Projects\WebSite4\TextFiler\");
+    TextFileModel model = new TextFileModel();
+
+    public WebService () {
+
+    }
+    
+    [WebMethod]
+    public string getTxtFile(string filename)
+    {
+        try 
+        { 
+            string text = model.getTxtFile(filename);
+            return text;
+        }
+        catch(SoapException e)
+        {
+            throw e;
+        }
+        catch(FileNotFoundException e)
+        {
+            throw e;
+        }
+        catch(Exception e)
+        {
+            throw e;
+        }
+    }
+
+    [WebMethod]
+    public List<string> getAllFileNames()
+    {
+        try
+        {
+            List<string> filenames = model.getAllFileNames();
+            return filenames;
+        }
+        catch (SoapException e)
+        {
+            throw e;
+        }
+        catch (FileNotFoundException e)
+        {
+            throw e;
+        }
+        catch (Exception e)
+        {
+            throw e;
+        }      
+    }
+
+    [WebMethod]
+    public void ChangeTxtFile(string fileText, string filename)
+    {
+        try
+        {
+            model.ChangeTxtFile(fileText, filename);
+        }
+        catch (SoapException e)
+        {
+            throw e;
+        }
+        catch (FileNotFoundException e)
+        {
+            throw e;
+        }
+        catch (Exception e)
+        {
+            throw e;
+        } 
+    }
+}
